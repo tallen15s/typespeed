15d878473fbdf5851f15a6c7ae2fe641921176c3
Tom Allen <tomallen851@yahoo.com>
2021-03-02 13:58:04 -0600

    change text size and added tailwinds color

--- src/components/Main/TimerForm/Form.js
+++ src/components/Main/TimerForm/Form.js
@@ -1,11 +1,11 @@
 function Form() {
   return (
     <form className="flex flex-col gap-3 items-center">
-      <label htmlFor="secs" className="text-2xl">
+      <label htmlFor="secs" className="text-3xl">
         How Long Should the Test Run?
       </label>
       <input type="number" id="secs" placeholder="secs" className="w-24" />
-      <button className="bg-neon-green cursor-pointer h-16 p-2 rounded-full text-2xl  text-gray-700 w-16 hover:animate-pulse focus:animate-pulse">
+      <button className="bg-blue-500 cursor-pointer h-16 p-2 rounded text-2xl  text-gray-700 w-16 hover:animate-pulse focus:animate-pulse">
         Go!
       </button>
     </form>

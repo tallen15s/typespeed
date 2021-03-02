d58b774bbcf43d6393106af84e1236b42d09a0f4
Tom Allen <tomallen851@yahoo.com>
2021-03-02 13:56:33 -0600

    add tailwinds for color

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import { Header, Main } from "./components";
 
 function App() {
   return (
-    <div className="flex flex-col font-mono gap-4 text-neon-green">
+    <div className="flex flex-col font-mono gap-4 text-blue-600">
       <Header />
       <Main />
     </div>

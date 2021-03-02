4191d8e13c0486e2464a38a6097f1ff61b0cce99
Tom Allen <tomallen851@yahoo.com>
2021-03-02 13:57:20 -0600

    add header name and add color

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,7 +1,5 @@
 function Header() {
-  return (
-    <header className="pt-4 text-7xl text-center">Typing Speed Test</header>
-  );
+  return <header className="pt-4 text-7xl text-center">Speed Test</header>;
 }
 
 export default Header;
